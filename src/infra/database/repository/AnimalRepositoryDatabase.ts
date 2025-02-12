7b6f58867aa278741733b0e1891e3e43d4a97d86
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-02-12 16:31:26 -0400

    :recycle: Add Error handler in Repository

--- src/infra/database/repository/AnimalRepositoryDatabase.ts
+++ src/infra/database/repository/AnimalRepositoryDatabase.ts
@@ -1,59 +1,68 @@
 import Animal from "../../../domain/entities/Animal";
 import AnimalRepository from "../../../domain/repository/Animal/AnimalRepository";
+import { BadRequestError } from "../../../shared/errors/Errors";
 import DatabaseConnection from "../config/DatabaseConnection";
 
 export default class AnimalRepositoryDatabase implements AnimalRepository {
   constructor(readonly databaseConnection: DatabaseConnection) {}
 
-  async create(animal: Animal): Promise<void> {
+  async create(animal: Animal): Promise<number | undefined> {
     try {
-      const hasAnimalWithThisId = await this.findById(animal.animalId);
-
-      if (hasAnimalWithThisId === false) {
-        await this.databaseConnection.query(
-          'INSERT INTO "Animal" (id, name, species, breed, age, size, status, description, image_url, location, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)',
-          [
-            animal.animalId,
-            animal.name,
-            animal.species,
-            animal.breed,
-            animal.age,
-            animal.size,
-            animal.status,
-            animal.description,
-            animal.image_url,
-            animal.location,
-            animal.user_id,
-          ]
-        );
-      }
+      const [res] = await this.databaseConnection.query(
+        'INSERT INTO "Animal" (id, name, species, breed, age, size, status, description, image_url, location, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id',
+        [
+          animal.animalId,
+          animal.name,
+          animal.species,
+          animal.breed,
+          animal.age,
+          animal.size,
+          animal.status,
+          animal.description,
+          animal.image_url,
+          animal.location,
+          animal.user_id,
+        ]
+      );
+      return res.id;
     } catch (error: unknown) {
-      console.log(error);
+      throw new BadRequestError({ cause: error });
     }
   }
 
-  async findById(id: number): Promise<Animal | Boolean> {
+  async find(id: number): Promise<Animal | Boolean> {
     try {
-      const animal = await this.databaseConnection.query(
+      const [animal] = await this.databaseConnection.query(
         'SELECT * FROM "Animal" WHERE id = $1',
         [id]
       );
 
       return {
-        animalId: +animal[0].id,
-        name: animal[0].name,
-        species: animal[0].species,
-        breed: animal[0].breed,
-        age: +animal[0].age,
-        size: animal[0].size,
-        status: animal[0].status,
-        description: animal[0].description,
-        image_url: animal[0].image_url,
-        location: animal[0].location,
-        user_id: animal[0].user_id,
+        animalId: +animal.id,
+        name: animal.name,
+        species: animal.species,
+        breed: animal.breed,
+        age: +animal.age,
+        size: animal.size,
+        status: animal.status,
+        description: animal.description,
+        image_url: animal.image_url,
+        location: animal.location,
+        user_id: animal.user_id,
       };
     } catch (error) {
       return false;
+    }
+  }
+
+  async delete(id: number): Promise<void> {
+    try {
+      await this.databaseConnection.query(
+        'DELETE FROM "Animal" WHERE id = $1',
+        [id]
+      );
+    } catch (error) {
+      throw new Error("Error to delete a animal");
     }
   }
 }
