0196bd5721e6f5bccad1e3baf9050247f06b808b
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-02-12 16:28:48 -0400

    :sparkles: Add delete Function to Interface AnimalRepository

--- src/domain/repository/Animal/AnimalRepository.ts
+++ src/domain/repository/Animal/AnimalRepository.ts
@@ -1,6 +1,7 @@
 import Animal from "../../entities/Animal";
 
 export default interface AnimalRepository {
-  create(animal: Animal): Promise<void>;
-  findById(id: number): Promise<Animal | Boolean>;
+  create(animal: Animal): Promise<number | undefined>;
+  find(id: number): Promise<Animal | Boolean>;
+  delete(id: number): Promise<void>;
 }
