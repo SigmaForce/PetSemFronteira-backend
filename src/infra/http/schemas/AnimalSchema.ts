67f6cb2f0bf77037edcd94b275298128c62b90b1
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-02-13 14:36:15 -0400

    :sparkles: Create 'AnimalSchema'  to  `/animal` requests

---
+++ src/infra/http/schemas/AnimalSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const createAnimalSchema = z.object({
+  name: z.string().min(1, "Name need to be defined"),
+  species: z.string().min(1, "Species need to be defined"),
+  breed: z.string().optional(),
+  gender: z.string().min(1, "Gender need to be defined"),
+  age: z.number().min(1, "Age need to be defined"),
+  size: z.string().min(1, "Size need to be defined"),
+  status: z.string().min(1, "Status need to be defined"),
+  description: z.string().min(1, "Description need to be defined"),
+  image_url: z.string().optional(),
+  location: z.string().optional(),
+  user_id: z.number().min(1, "User_id need to be defined"),
+});
