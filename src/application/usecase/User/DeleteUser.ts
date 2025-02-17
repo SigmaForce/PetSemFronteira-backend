69bae2ed9867a9816ddab224ff3094a003acc68d
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-02-17 17:08:37 -0400

    :recycle: refactoring change delete email per id

--- src/application/usecase/User/DeleteUser.ts
+++ src/application/usecase/User/DeleteUser.ts
@@ -3,7 +3,7 @@ import UserRepository from "../../../domain/repository/User/UserRepository";
 export default class DeleteUser {
   constructor(readonly userRepository: UserRepository) {}
 
-  async execute(email: string): Promise<void> {
-    await this.userRepository.delete(email);
+  async execute(id: string): Promise<void> {
+    await this.userRepository.delete(id);
   }
 }
