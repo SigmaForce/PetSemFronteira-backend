982b6db1e150f5c7e34f626c433db610a873865f
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-02-17 17:10:05 -0400

    :recycle: refactoring: add user routes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import AnimalRoutes from "./infra/http/routes/AnimalRoutes";
+import UserRoutes from "./infra/http/routes/UserRoutes";
 
 async function main() {
   const app = express();
   app.use(express.json());
   app.use("/animal", AnimalRoutes);
+  app.use("/user", UserRoutes);
 
   app.listen(3000);
 }
