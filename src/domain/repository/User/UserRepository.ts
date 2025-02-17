0259ccb9a64ab502607e5ab6f52e77b4be180858
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-02-17 17:07:23 -0400

    :recycle: refactoring: Add other methods to `userRepository`

--- src/domain/repository/User/UserRepository.ts
+++ src/domain/repository/User/UserRepository.ts
@@ -2,8 +2,8 @@ import User from "../../entities/User";
 
 export default interface UserRepository {
   create(user: User): Promise<string | undefined>;
-  //findByID(id: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   //update(user: User): Promise<string | undefined>;
-  delete(email: string): Promise<void>;
+  delete(id: string): Promise<void>;
 }
