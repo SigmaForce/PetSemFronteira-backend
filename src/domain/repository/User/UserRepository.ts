2d62b013e418e1300757a1522fcc647ad03559a3
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-02-26 14:52:34 -0400

    :recycle: Add update function to UserRepository Interface

--- src/domain/repository/User/UserRepository.ts
+++ src/domain/repository/User/UserRepository.ts
@@ -4,6 +4,6 @@ export default interface UserRepository {
   create(user: User): Promise<string | undefined>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
-  //update(user: User): Promise<string | undefined>;
+  update(user: User): Promise<void>;
   delete(id: string): Promise<void>;
 }
