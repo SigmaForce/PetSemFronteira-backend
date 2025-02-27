99bc39fa9f46db34f39ff02d6d994bfb47cc14a0
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-02-27 15:28:11 -0400

    :recycle: add: new routes to userRoutes

--- src/infra/http/routes/UserRoutes.ts
+++ src/infra/http/routes/UserRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {
   authenticateUser,
+  changePassword,
   createUser,
   deleteUser,
   getUser,
+  updateUser,
 } from "../controllers/UserController";
 
 const UserRoutes = Router();
@@ -12,5 +14,7 @@ UserRoutes.get("/:id", getUser);
 UserRoutes.post("/signIn", authenticateUser);
 UserRoutes.post("/", createUser);
 UserRoutes.delete("/:id", deleteUser);
+UserRoutes.put("/changePassword/:id", changePassword);
+UserRoutes.put("/:id", updateUser);
 
 export default UserRoutes;
