86cf3465ead622855269b572819a3cfa4030bc41
Leonardo Lucas <leonardolucas2020@gmail.com>
2025-02-27 15:28:51 -0400

    :sparkles: create: UpdateUser Controller and move databaseConnection.close to finally block

--- src/infra/http/controllers/UserController.ts
+++ src/infra/http/controllers/UserController.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
 import { z, ZodError } from "zod";
 import AuthenticateUser from "../../../application/usecase/User/AuthenticateUser";
+import ChangePassword from "../../../application/usecase/User/ChangePassword";
 import CreateUser from "../../../application/usecase/User/CreateUser";
+import { UpdateUser } from "../../../application/usecase/User/UpdateUser";
 import { BadRequestError } from "../../../shared/errors/Errors";
+import { isEmpty } from "../../../shared/utils/IsEmpty";
 import { pgPromiseAdapter } from "../../database/config/DatabaseConnection";
 import UserRepositoryDatabase from "../../database/repository/UserRepositoryDatabase";
+import { ChangePasswordSchema } from "../schemas/ChangePasswordSchema";
 import { createUserSchema } from "../schemas/CreateUserSchema";
+import { UpdateUserSchema } from "../schemas/UpdateUserSchema";
 
 const paramSchema = z.object({
   id: z.string().min(1, "Need to provide a identifier."),
@@ -17,6 +22,8 @@ const signInSchema = z.object({
 });
 
 export const authenticateUser = async (req: Request, res: Response) => {
+  const databaseConnection = new pgPromiseAdapter();
+  const userRepository = new UserRepositoryDatabase(databaseConnection);
   try {
     let input;
     try {
@@ -29,25 +36,26 @@ export const authenticateUser = async (req: Request, res: Response) => {
       throw err;
     }
 
-    const databaseConnection = new pgPromiseAdapter();
-    const userRepository = new UserRepositoryDatabase(databaseConnection);
     const auth = new AuthenticateUser(userRepository);
 
     const token = await auth.execute(input);
     res.status(200).json(token);
-    databaseConnection.close();
   } catch (err: any) {
     console.error(err);
-    res.status(err.statusCode).json({
+    res.status(err.statusCode || 500).json({
       name: err.name,
       message: err.message,
       action: err.action,
       status: err.statusCode,
     });
+  } finally {
+    databaseConnection.close();
   }
 };
 
 export const createUser = async (req: Request, res: Response) => {
+  const databaseConnection = new pgPromiseAdapter();
+  const userRepository = new UserRepositoryDatabase(databaseConnection);
   try {
     let input;
     try {
@@ -58,27 +66,28 @@ export const createUser = async (req: Request, res: Response) => {
       }
       throw err;
     }
-    const databaseConnection = new pgPromiseAdapter();
-    const userRepository = new UserRepositoryDatabase(databaseConnection);
     const createUser = new CreateUser(userRepository);
     const userId = await createUser.execute(input);
 
     res.status(201).json({
       userId: userId?.userId,
     });
-    databaseConnection.close();
   } catch (err: any) {
     console.error(err);
-    res.status(err.statusCode).json({
+    res.status(err.statusCode || 500).json({
       name: err.name,
       message: err.message,
       action: err.action,
       status: err.statusCode,
     });
+  } finally {
+    databaseConnection.close();
   }
 };
 
 export const getUser = async (req: Request, res: Response) => {
+  const databaseConnection = new pgPromiseAdapter();
+  const userRepository = new UserRepositoryDatabase(databaseConnection);
   try {
     let input;
     try {
@@ -90,26 +99,27 @@ export const getUser = async (req: Request, res: Response) => {
       throw err;
     }
 
-    const databaseConnection = new pgPromiseAdapter();
-    const userRepository = new UserRepositoryDatabase(databaseConnection);
     const user = await userRepository.findById(input.id);
 
     if (!user) res.status(204).json(user);
 
     if (user) res.status(200).json(user);
-    databaseConnection.close();
   } catch (err: any) {
     console.error(err);
-    res.status(err.statusCode).json({
+    res.status(err.statusCode || 500).json({
       name: err.name,
       message: err.message,
       action: err.action,
       status: err.statusCode,
     });
+  } finally {
+    databaseConnection.close();
   }
 };
 
 export const deleteUser = async (req: Request, res: Response) => {
+  const databaseConnection = new pgPromiseAdapter();
+  const userRepository = new UserRepositoryDatabase(databaseConnection);
   try {
     let input;
     try {
@@ -120,18 +130,101 @@ export const deleteUser = async (req: Request, res: Response) => {
       }
       throw err;
     }
-    const databaseConnection = new pgPromiseAdapter();
-    const userRepository = new UserRepositoryDatabase(databaseConnection);
+
     await userRepository.delete(input.id);
-    databaseConnection.close();
     res.status(204).send();
   } catch (err: any) {
     console.error(err);
-    res.status(err.statusCode).json({
+    res.status(err.statusCode || 500).json({
       name: err.name,
       message: err.message,
       action: err.action,
       status: err.statusCode,
     });
+  } finally {
+    databaseConnection.close();
+  }
+};
+
+export const changePassword = async (req: Request, res: Response) => {
+  const databaseConnection = new pgPromiseAdapter();
+  const userRepository = new UserRepositoryDatabase(databaseConnection);
+  try {
+    const changePassword = new ChangePassword(userRepository);
+
+    let input = undefined;
+    let userId = undefined;
+    try {
+      input = ChangePasswordSchema.parse(req.body);
+      userId = paramSchema.parse({ id: req.params.id });
+    } catch (err) {
+      if (err instanceof ZodError) {
+        throw new BadRequestError({ cause: err });
+      }
+      throw err;
+    }
+
+    const { oldPassword, newPassword } = input;
+
+    await changePassword.execute({
+      userId: userId.id,
+      oldPassword,
+      newPassword,
+    });
+
+    res.status(200).send({
+      message: "Password changed successfully",
+    });
+  } catch (err: any) {
+    res.status(err.statusCode || 500).json({
+      name: err.name,
+      message: err.message,
+      action: err.action,
+      status: err.statusCode,
+    });
+  } finally {
+    databaseConnection.close();
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  const databaseConnection = new pgPromiseAdapter();
+  const userRepository = new UserRepositoryDatabase(databaseConnection);
+  try {
+    const updateUser = new UpdateUser(userRepository);
+    let input = undefined;
+    let userId = undefined;
+    try {
+      input = UpdateUserSchema.parse(req.body);
+      userId = paramSchema.parse({ id: req.params.id });
+    } catch (err) {
+      if (err instanceof ZodError) {
+        throw new BadRequestError({ cause: err });
+      }
+      throw err;
+    }
+
+    if (isEmpty(input)) {
+      res.status(204).send();
+    }
+
+    let userData = {
+      userId: userId.id,
+      ...input,
+    };
+
+    await updateUser.execute(userData);
+
+    res.status(204).send();
+  } catch (err: any) {
+    console.error(err);
+    res.status(err.statusCode || 500).json({
+      name: err.name,
+      message: err.message,
+      action: err.action,
+      status: err.statusCode,
+    });
+  } finally {
+    databaseConnection.close();
   }
 };
